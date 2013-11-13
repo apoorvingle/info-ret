38cb2171fb17b404b5c5483d4beaa0978242e494
apoorvingle <apoorv.ingle@gmail.com>
2013-11-14 05:28:23 +0530

    reading corpus test

--- README.md
+++ README.md
@@ -2,11 +2,17 @@ N-Gram Retrieval
 ================
 
 - Generate 5 grams from corpus
-- Generate long-llog plot of frequency vs rank order
+- Generate long-log plot of frequency vs rank order
 - Show whether 5-grams follow Zipf's law and approximate value of alpha
 
 - To Do
     - Know the structure of the corpus
     - Build a parsing mechanism
-    - extract the tokens
-    - N-gram stuff
+        - parallel mechanism
+        - make 10 threads work parallely to get all the data and write it down
+          in a file
+        - 
+    - extract the tokens and generate ngrams
+
+    - generate log-log plot of frequency vs rank order using matplotlib
+
--- parse.py
+++ parse.py
@@ -1,36 +1,71 @@
+import os
 import glob
+import threading
 from nltk.util import ngrams
 from nltk.stem.porter import PorterStemmer
 
-rawData = []
-#open files
-lOfFiles = glob.glob("corpusr/*.txt")
-for docs in lOfFile:
-    try:
-        f = open(docs)
-        rawData.append(f.read())
-        f.close()
-    except:
-        pass
+#Stemmer to stem the words
+ps = PorterStemmer()
 
+def digestData(rawData):
+    """generates the ngrams of the given text
+        | input : raw data read from the file stream
+        | output: ngrams of the given input
+    """
+    #strip the punctuations
+    finerText = str(rawData).lower().translate(None, "!?*+@#$%^&*()<>\"\':;.,-|_\n\r")
+    #stem the data
+    stemmedText = ps.stem(finerText)
+    #generate character 5-grams of the stemmed text 
+    return ngrams(list(stemmedText),5)
 
-#read data 
-#rawData = str(f.read())
+def digestFile(inFilePath, outFilePath):
+    """reads file from inFilePath digests data and outputs
+       the data in the outFilePath folder
+       | input : path to the input file to read data inFilePath, 
+                 path to the output file to write data outFilePath
+       | output: None
+    """
+    #open input stream
+    inFile = open(inFilePath)
 
-#get the documents from the file
+    #open output stream
+    outFile = open(outFilePath, "w")
+    
+    #read inputstream
+    rawData = inFile.read()
+    outFile.write(digestData(rawData))
 
-#strip the text off from punctuations
+def digestMultipleFiles(inFileList, outFilePath):
+    """digests multiple files given in the inFileList
+       and generates corresponding data in the outFilePath
+       | input : digest the files given in the inFileList
+       | output: None
+    """
+    for fileName in inFileList:
+        digestFile(fileName, outFilePath+fileName)
 
-#stemmer
-#ps = PorterStemmer()
-#text = ps.stem(text)
+def digestAllFiles(inDirectory, outDirectory):
+    """uses 10 parallel threads to read 
+       all the files in the inDirectory
+       digests data and writes in the outDirectory
+       each file in the outDirectory contains the respective
+       ngrams of each file
+       | input : directory name in which the corpus is situated
+       | output: None
+    """
+    path = dirname + "/"
+    thread_list=[]
+    for i in range(1,10):
+        listOfFiles = glob.glob(inDirectory+"/"+str(i)+"*")
+        t = threading.Thread(target=digestMultipleFiles, args=(listOfFiles, outDirectory))
+        thread_list.append(t)
 
-#tokenize
-#l = list(text)
-#build character ngrams from the document
+    #start the thread execution
+    for thread in thread_list:
+        thread.start()
+    
+    #wait for the threads to stop
+    for thread in thread_list:
+        thread.join()
 
-#n = 5
-#fiveGrams = ngrams(l,n)
-#for grams in fiveGrams:
-#    print grams
-#
